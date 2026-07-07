2987130859e113409dcbc56f3340bc8829c3ca18
OpenInspect <open-inspect@noreply.github.com>
2026-07-07 23:48:30 +0000

    change sign-in heading to 'Welcome back'

--- apps/web/e2e/tests/auth.e2e.ts
+++ apps/web/e2e/tests/auth.e2e.ts
@@ -2,7 +2,7 @@ import { clerk } from "@clerk/testing/playwright";
 import { expect, test } from "@playwright/test";
 
 function getSignInPromptMatcher() {
-  return /sign in/i;
+  return /welcome back/i;
 }
 
 test.describe("Authentication", () => {
